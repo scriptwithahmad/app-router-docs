3bbafea0816a5605f95e65be41bcfcc713ed5aa2
Muhammadh Ahmad <scriptwithahmad@gmail.com>
2024-01-27 14:18:58 +0500

    add notFound page of Next js - built in

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>Page not accessable</p>
+    </div>
+  );
+};
+
+export default notFound;
