d2f2c2ee8a4c6bb500d7dd4de6fed63439ad8ebe
Muhammadh Ahmad <scriptwithahmad@gmail.com>
2024-02-17 11:36:51 +0500

    pulling data

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Inter, Open_Sans } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const sans = Open_Sans({
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={sans.className}>{children}</body>
     </html>
   );
 }
